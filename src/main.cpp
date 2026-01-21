1971e7fac4e5e117dc2d217bedc262129dde9454
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-01-21 13:07:12 -0500

    let's go(?)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GameStatsManager.hpp>
 
 using namespace geode::prelude;
+
+class $modify(PlayLayer) {
+	void playEndAnimationToPos(cocos2d::CCPoint position) {
+		bool origTestmode = this->m_isTestMode;
+		this->m_isTestMode = true;
+		PlayLayer::playEndAnimationToPos(position);
+		this->m_isTestMode = origTestmode;
+	}
+	void levelComplete() {
+		bool origTestmode = this->m_isTestMode;
+		this->m_isTestMode = true;
+		PlayLayer::levelComplete();
+		this->m_isTestMode = origTestmode;
+	}
+};
 
 class $modify(GameStatsManager) {
 	void setStat(char const* key, int value) {
